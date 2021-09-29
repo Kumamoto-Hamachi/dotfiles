fcbfdfdd97b641d256a1f6c89941191154882f2f
Kumamoto <onioni9999nagare@gmail.com>
2021-09-29 17:58:48 +0900

    update/first_shot for nvim

--- first_shot.sh
+++ first_shot.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 #conf_array=(vim bash) #TODO
 readonly CONFDIR=${PWD}
-ln -s ${CONFDIR}/vim/.vimrc ${HOME}/.vimrc
 ln -s ${CONFDIR}/bash/.bashrc ${HOME}/.bashrc
 ln -s ${CONFDIR}/bash/.bash_profile ${HOME}/.bash_profile
 ln -s ${CONFDIR}/tmux/.tmux.conf ${HOME}/.tmux.conf
+ln -s ${CONFDIR}/vim/.vimrc ${HOME}/.vimrc
+mkdir -p ${HOME}/.config/nvim
+ln -s ${CONFDIR}/vim/init.vim ${HOME}/.config/nvim/init.vim
