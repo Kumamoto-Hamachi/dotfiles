dd6dba32e400522ac8e68094226063aed6cbb2f8
kumamoto <onioni9999nagare@gmail.com>
2021-10-25 23:28:55 +0900

    add/bash_func

---
+++ bash/functions/python_migrater/migration_waitor.bash
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+source ../check_array.bash # TODO これ雑
+
+migration_waitor() {
+	local order_array=($1)
+	while :
+	do
+		echo -e "へい, いらっしゃい! どのマイグレートにしやしょうか?
+		python manage.py
+		0. 終了
+		1. テーブル定義反映(migrate APP名)
+		2. 過去に実行済のmigration一覧(showmigrations APP名)
+		3. 特定の地点までmigrationを戻す(migrate APP名 migration名)
+		9. (\e[0;31m Warning \e[m)マイグレーションを実施していない状態に戻す(migrate APP名 zero)
+		"
+		printf "オーダー: "
+		read order
+		if [ $order = "0" ]; then
+			echo "終了するね"
+			exit
+		elif check_array $order "${order_array[*]}"; then
+			printf "OK, APP名を教えてくれ(showの時はanyもOK): "
+			read app_name
+			return
+		else
+			echo -e "おいおい, そんな\e[0;31m $order \e[mみたいなオーダーは受け付けられないぜ."
+		fi
+	done
+}
