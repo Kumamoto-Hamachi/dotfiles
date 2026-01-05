dc591dc788b2cd3eba142b2da8d4e350bc5f0b8c
kumamoto <onioni9999nagare@gmail.com>
2026-01-05 14:30:58 +0900

    lazy.nvimとpython関連で必要なpluginのミニマルな設定追加

--- _.config/nvim/init.lua
+++ _.config/nvim/init.lua
@@ -2,9 +2,29 @@
 
 -- Leaderキーを先に設定（キーマップより前に必要）
 vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 -- 既存の.vimrc設定を読み込む
 vim.cmd('source ~/.vimrc')
+
+--------------------------------------------------------------------------------
+-- lazy.nvim ブートストラップ
+--------------------------------------------------------------------------------
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- プラグイン設定を読み込む
+require("lazy").setup("plugins")
 
 --------------------------------------------------------------------------------
 -- VSCode / 通常Neovim の分岐
