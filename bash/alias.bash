e365911982e26219abb520cd731421eb5f96059f
kumamoto <onioni9999nagare@gmail.com>
2026-03-02 15:34:43 +0900

    update

--- bash/alias.bash
+++ bash/alias.bash
@@ -31,7 +31,8 @@ alias cf='cd $(ghq list --full-path | fzf)'
 #---------------------------------------
 alias ai='$(which claude)'
 alias serena-mcp-add='claude mcp add serena -- uvx --from git+https://github.com/oraios/serena serena start-mcp-server --context claude-code --project "$(pwd)"'
-alias terraform-mcp-add='claude mcp add terraform-mcp -s project --transport stdio -- docker run -i --rm hashicorp/terraform-mcp-server:0.3.0'
+alias terraform-mcp-add='claude mcp add terraform-mcp -s project --transport stdio -- docker run -i --rm hashicorp/terraform-mcp-server:0.4.0'
+alias sentry-mcp-add='claude mcp add --transport http sentry https://mcp.sentry.dev/mcp'
 #---------------------------------------
 
 # apt
